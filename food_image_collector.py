7ca10ba07a8258b36530129ef27583748172090b
PaingLinHtike <plhcumaubin1010@gmail.com>
2026-06-20 23:09:52 +0630

    Add image upload workflow

--- food_image_collector.py
+++ food_image_collector.py
@@ -5,13 +5,16 @@ from PIL import Image
 from save_to_gsheets import append_values_to_gsheet
 from utils import create_unique_filename
 from rich import print, pretty, traceback
+
 pretty.install()
 traceback.install()
 
 st.title("Food Vision Image Collector 🍔")
 st.write("Upload images of food to help us build a delicious dataset!")
 
-uploaded_images = st.file_uploader("Upload an image of food", type=["jpg", "jpeg", "png"])
+# Increase upload size
+uploaded_images = st.file_uploader("Upload an image of food", type=["jpg", "jpeg", "png"], max_upload_size=200)
+
 
 # Display the uploaded image if available
 def display_image(img):
@@ -19,24 +22,41 @@ def display_image(img):
         image = Image.open(img)
         print("Displaying Image....")
         st.image(image, caption="Uploaded Image", width=300)
-        return image 
+        return image
     return None
 
-image = display_image(uploaded_images)   
+
+image = display_image(uploaded_images)
 
 # Create Image Label Form
-st.write("### Image Labeling")
+with st.form(key="label_submit_form", clear_on_submit=True):
+    st.write("### Image Labeling")
+    label = st.text_input(
+        "What food(s) are in the image you uploaded? \
+                            You can enter text like 'pizza', 'burger', etc.:",
+        max_chars=200,
+        icon="🍔",
+        placeholder="Burger",
+    )
+    country = st.text_input(
+        "Which country is this food from? (optional)",
+        max_chars=100,
+        placeholder="Myanmar",
+        icon="🏠",
+    )
+    email = st.text_input(
+        "Your email (optional, for updates on the project)",
+        max_chars=100,
+        autocomplete="email",
+        icon="📧",
+        placeholder="user@gmail.com",
+    )
 
-form = st.form(key = "label_submit_form", clear_on_submit=True)
-label = form.text_input("What food(s) are in the image you uploaded? \
-                        You can enter text like 'pizza', 'burger', etc.:", max_chars=200)
-country = form.text_input("Which country is this food from? (optional)", max_chars=100)
-email = form.text_input("Your email (optional, for updates on the project)", max_chars=100, autocomplete="email")
-
-form.markdown("**Note:** If you click upload image, your image will be stored on \
-        Nutrify servers and used to create the largest food image database \
-        in the world!")
-submit_button = form.form_submit_button("Upload Image", help="Click to upload the image and label to Nutrify server.")
+    st.info("**Note:** If you click upload image, your image will be stored on. Nutrify servers and used to create the largest food image database in the world!")
+    submit_button = st.form_submit_button(
+        "Upload Image",
+        help="Click to upload the image and label to Nutrify server.",
+    )
 
 if submit_button:
     # Check if an image has been uploaded
@@ -53,15 +73,28 @@ if submit_button:
     # Upload the image to Google Cloud Storage
     # bucket_name = "food-vision-project-images"
     destination_blob_name = f"{unique_image_id}.jpg"
-    try:
-        upload_blob(uploaded_images, destination_blob_name, content_type="image/jpeg")
-    except RuntimeError as error:
-        st.error(str(error))
-        st.stop()
-    
+    with st.spinner("Uploading image to server..."):
+        try:
+            upload_blob(
+                uploaded_images,
+                destination_blob_name,
+                content_type="image/jpeg",
+            )
+        except RuntimeError as error:
+            st.error(str(error))
+            st.stop()
+
     # Store metadata in Google Sheets
     img_width, img_height = image.size
-    image_info = [unique_image_id, current_time, img_height, img_width, email, country, label]
+    image_info = [
+        unique_image_id,
+        current_time,
+        img_height,
+        img_width,
+        email,
+        country,
+        label,
+    ]
     try:
         response = append_values_to_gsheet([image_info])
     except (PermissionError, RuntimeError) as error:
@@ -72,3 +105,26 @@ if submit_button:
     print("Image Info:", image_info)
 
     st.success("Image uploaded successfully! Thank You🙏")
+
+st.write("## FAQ")
+with st.expander("What happens to my image?"):
+    st.write("""
+    When you click "upload image", your image gets stored on Nutrify servers\
+         (a big hard drive on Google Cloud).\n
+    Here's a pretty picture which describes it in more detail:
+    """)
+    st.image("./images/image-uploading-workflow-with-background.png")
+    st.write("Later on, images in the database will be used to train a computer \
+            vision model to power Nutrify.")
+with st.expander("Why do you need images of food?"):
+    st.write("""
+    Machine learning models learn by looking at many different examples \
+        of things.\n
+    Food included.\n
+    Eventually, Nutrify wants to be an app you can use to *take a photo of \
+        food and learn about it*.\n
+    To do so, we'll need many different examples of foods to build a \
+        computer vision
+    model capable of identifying almost anything you can eat.\n
+    And the more images of food you upload, the better the models will get.
+    """)
